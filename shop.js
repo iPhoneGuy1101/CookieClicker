f4b4312f71c2029dbe0d73c32147c6d3600d213b
iPhoneGuy1101 <johnj1101@icloud.com>
2017-03-31 09:01:07 -0500

    Add files via upload

---
+++ shop.js
@@ -0,0 +1,13 @@
+var shop = [
+	{name: "Cursor",                img: "cursor.png",       cost: 15,          cps: 0.1             },
+	{name: "Grandma",               img: "grandma.png",      cost: 100,         cps: 1               },
+	{name: "Cookie Tree",           img: "tree.png",         cost: 300,         cps: 3               },
+	{name: "Farm",                  img: "farm.png",         cost: 450,         cps: 5               },
+	{name: "Mine",                  img: "mine.png",         cost: 1000,        cps: 10              },
+	{name: "Cookie Planet",         img: "planet.png",       cost: 10000,       cps: 111             },
+	{name: "Prism",                 img: "prism.png",        cost: 100000,      cps: 1234            },
+	{name: "Cookie Converter",      img: "converter.png",    cost: 1000000,     cps: 12345           },
+	{name: "Portal to Cookiverse",  img: "cookieverse.png",  cost: 11011101,    cps: 110101          },
+	{name: "Cookie Fireworks",      img: "fireworks.png",    cost: 123456789,   cps: 1234567         },
+	{name: "SuperCursor!",          img: "supercursor.png",  cost: 1000000000,  cps: 0,        cpc: 3}
+];
